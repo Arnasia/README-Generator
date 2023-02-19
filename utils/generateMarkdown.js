2284ab084a59270cc37aa6ed98e7b64e8f0b4a95
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-02-18 20:45:06 -0500

    add function to return a license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return`![badge](https://img.sheilds.io/badge/license-${license}-blue)`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -20,6 +26,7 @@ function generateMarkdown(data) {console.log(data)
   ## DESCRIPTION
   # ${data.description}
 
+ 
 `;
 }
 
